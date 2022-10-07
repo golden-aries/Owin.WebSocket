4c32376076ea456ac4c991cdee019d7dc14609ad
Sergey Ermolenko <sergey.ermolenkoo@gmail.com>
2022-10-07 14:12:10 -0700

    websocket sample

--- src/OwsDemo/Startup.cs
+++ src/OwsDemo/Startup.cs
@@ -1,13 +1,15 @@
 ﻿using Owin;
 using Owin.WebSocket.Extensions;
 using Microsoft.Owin;
+using Microsoft.Owin.StaticFiles;
+
 namespace OwsDemo
 {
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            //app.UseDefaultFiles("/");
+            app.UseDefaultFiles();
             app.MapWebSocketRoute<MyWebSocket>("/ws");
             app.Run(context =>
             {
