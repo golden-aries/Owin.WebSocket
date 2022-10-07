8fe7b62e1584e7b4b059048b13136f15bdcb3c28
Sergey Ermolenko <sergey.ermolenkoo@gmail.com>
2022-10-07 13:06:29 -0700

    OwsDemo

---
+++ src/OwsDemo/MyWebSocket.cs
@@ -0,0 +1,23 @@
+﻿using Owin.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwsDemo
+{
+    public class MyWebSocket : WebSocketConnection
+    {
+        public override Task OnMessageReceived(ArraySegment<byte> message, WebSocketMessageType type)
+        {
+            //Handle the message from the client
+
+            //Example of JSON serialization with the client
+            //var json = Encoding.UTF8.GetString(message.Array, message.Offset, message.Count);
+            //Use something like Json.Net to read the json
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/OwsDemo/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using Owin.WebSocket.Extensions;
+namespace OwsDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapWebSocketRoute<MyWebSocket>();
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
